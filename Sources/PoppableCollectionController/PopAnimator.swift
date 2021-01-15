44c90e0583b787f7ce4e014e819d8f3ffa56ce89
Garrett <garrett@wrldsapp.com>
2021-01-15 08:44:25 -0500

    Update

--- Sources/PoppableCollectionController/PopAnimator.swift
+++ Sources/PoppableCollectionController/PopAnimator.swift
@@ -89,11 +89,9 @@ public class PopAnimator: NSObject, UIViewControllerAnimatedTransitioning {
         let containerOrigin = fromView.convert(screenshotToView.frame.origin, to: container)
         screenshotToView.frame.origin = containerOrigin
         
-        var endFrame = screenshotToView.convert(toView.frame, to: container)
+        var endFrame = toView.convert(toView.frame, to: container)
         endFrame.origin.y = topInset + 20
 
-        print("END FRAME \(endFrame)")
-        
         // If a source image is specified, use it for the transition,
         // otherwise create a screenshot of the cell's content.
         let screenshotFromView = UIImageView(image: fromVC.sourceImage ?? currentCell.screenshot)
